9775f3b89ad70ddb4f91faf0c85f200c62284ed9
evertonpetrin <everton.petrin@gmail.com>
2020-08-01 12:02:57 -0300

    ok

--- src/app/importacao/importacao.module.ts
+++ src/app/importacao/importacao.module.ts
@@ -3,10 +3,13 @@ import { ImportacaoRoutingModule } from './importacao-routing/importacao-routing
 import { ImportacaoComponent } from './importacao.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 @NgModule({
     imports: [
         ImportacaoRoutingModule,
         CommonModule,
+        TabsModule.forRoot(),
         FormsModule
     ],
     declarations: [ImportacaoComponent],
